e4a2f514b750448b73b5214b64e3dfdb405e7218
Hebert Gustavo Gómez Quiroga <heggomez@bancolombia.com.co>
2024-03-13 06:34:13 -0500

    Aplicar ajuste cors2

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,20 +1,21 @@
 import cors from 'cors'
-export const corsMiddleware = () => corsMiddleware({
-    origin:(origin, callback) => {
-        const ACCEPTED_ORIGINS = [
-          'http://localhost:8080',
-          'http://localhost:3245',
-          'https://clientes.com'
-        ]
-    
-        if (ACCEPTED_ORIGINS.includes(origin)) {
-          return callback(null, true)
+
+const ACCEPTED_ORIGINS = [
+    'http://localhost:8080',
+    'http://localhost:3245',
+    'https://clientes.com'
+]
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
+    origin: (origin, callback) => {
+        if (acceptedOrigins.includes(origin)) {
+            return callback(null, true)
         }
-    
+
         if (!origin) {
-          return callback(null, true)
+            return callback(null, true)
         }
-    
+
         return callback(new Error('Not allowed by CORS'))
-      }
+    }
 })
